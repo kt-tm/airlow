2d389a1a4ac333f757cb16d651c24dcf1e758106
kt-tm <katya.melnikowa@mail.ru>
2021-04-07 20:58:06 +0300

    Lession 3

--- lession-3/example_dag.py
+++ lession-3/example_dag.py
@@ -1,21 +1,15 @@
-import task.task_body as t
 from airflow.models import DAG
 from util.settings import default_settings
-from util.dummy import dummy
 from util.bash import bash
-from util.deco import python_operator
-
-
-
-# @python_operator()
-# def decorated_python_operator_definition(**context):
-#     print('Hey I\'m a decorated operator.')
+import util.task_body as t
 
 with DAG(**default_settings()) as dag:
-    bash('first_task', 'echo "Here we start! Info: run_id={{ run_id }} | dag_run={{ dag_run }}"') >> t.download_titanic_dataset() >> (t.pivot_dataset(), t.mean_fare_per_class()) >> bash('last_task', 'echo "Pipeline finished! Execution date is {{ ds }}"')
+    first_task = bash('first_task', 'echo "Here we start! Info: run_id={{ run_id }} | dag_run={{ dag_run }}"')
+    last_task = bash('last_task', 'echo "Pipeline finished! Execution date is {{ ds }}"')
 
-        # with DAG(**default_settings()) as dag:
-#     decorated_python_operator_definition()
-# with DAG(**default_settings()) as dag:
-#     dummy('first_task') >> dummy('create_titanic_dataset') >> \
-#         (dummy('pivot_titanic_dataset'), dummy('mean_fares_titanic_dataset')) >> dummy('last_task')
+    (
+            first_task >>
+            t.download_titanic_dataset() >>
+            (t.pivot_dataset(), t.mean_fare_per_class()) >>
+            last_task
+    )
--- lession-3/util/bash.py
+++ lession-3/util/bash.py
@@ -2,7 +2,7 @@ from airflow.operators.bash_operator import BashOperator
 
 
 def bash(task_id, bash_command, **context):
-    return BashOperator(task_id=task_id, bash_command=bash_command, dag=dag)
+    return BashOperator(task_id=task_id, bash_command=bash_command)
 #
 # BashOperator(
 #         task_id='first_task',
